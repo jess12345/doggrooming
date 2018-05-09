6cdce6034c133dd8a9d8bcbb08c1f81b39714763
Yicong Li <liyicong.china@gmail.com>
2018-05-09 14:44:02 +1000

    Add partial communication protocal
    Except Dog and appointment

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -10,77 +10,149 @@ export const userService = {
     delete: _delete
 };
 
-function login(username, password) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
+// Groomer request
+function getAllGroomer() {
+    return postRequest('/Groomer.svc/ViewAll')
+}
+
+function getInfoOfGroomer(index) {
+    return postRequest('/Groomer.svc/View/' + index);
+}
+
+function addNewGroomer(firstName, lastName, email, password) {
+    var groomerInfo = firstName + '/' + lastName + '/' + email + '/' + password;
+    return postRequest('/Groomer.svc/Add/' + groomerInfo)
+}
+
+function updateGroomer( index, firstName, lastName, email, password) {
+    var groomerInfo = firstName + '/' + lastName + '/' + email + '/' + password;
+    return postRequest('/Groomer.svc/Update/' + index + '/'+ groomerInfo)
+}
+
+function loginGroomer(email, password) {
+        const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
     };
 
-    return fetch(config.apiUrl + '/users/authenticate', requestOptions)
-        .then(handleResponse, handleError)
-        .then(user => {
+    return fetch(config.apiUrl + '/Groomer.svc/Authenticate/' + email + '/'+ password , requestOptions)
+            .then(handleResponse, handleError);
+            .then(user => {
             // login successful if there's a jwt token in the response
-            if (user && user.token) {
+                if (user && user.token) {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
-            }
+                    localStorage.setItem('groomer', JSON.stringify(user));
+                }
 
-            return user;
+                return user;
         });
+}
+
+function deleteGroomer(index) {
+    return postRequest('/Groomer.svc/Delete/' + index);
+}
+
+//Breed
+
+function getAllBreed() {
+    return postRequest('/Breed.svc/ViewAll');
+}
+
+function getInfoOfBreed(index) {
+    return postRequest('/Breed.svc/View/' + index);
+}
+
+function addBreed(name) {
+    return postRequest('/Breed.svc/Add/' + name);
+}
+
+function deleteBreed(index) {
+    return postRequest('/Breed.svc/Delete/' + index);
+}
+
+// Grooming type
+
+function getAllGroomingType () {
+    return postRequest('/GroomingType.svc/ViewAll');
+}
+
+function getInfoOfGroomingType (index) {
+    return postRequest('/GroomingType.svc/View/' + index);
+}
+
+function AddGroomingType (name) {
+    return postRequest('/GroomingType.svc/Add/' + name);
+}
+
+function deleteGroomingType (index) {
+    return postRequest('/GroomingType.svc/Delete/' + index);
+}
+
+// client
+function getAllClientsInfo() {
+    return postRequest('/Client.svc/ViewAll');
+}
+
+function getClientInfo(index) {
+    return postRequest('/Client.svc/View/' + index);
+}
+
+function addClient(firstname, surname, email, password, homeAddress, mobilePh, workPhone, homePhone) {
+    var info = firstname + '/' + surname + '/' + email + '/' + password + '/' 
+        + homeAddress + '/' + mobilePh + '/' + workPhone+ '/' + homePhone;
+    return postRequest('/Client.svc/Add/' + info);
+}
+
+function updateClient(firstname, surname, email, password, homeAddress, mobilePh, workPhone, homePhone) {
+    var info = firstname + '/' + surname + '/' + email + '/' + password + '/' 
+        + homeAddress + '/' + mobilePh + '/' + workPhone+ '/' + homePhone;
+    return postRequest('/Client.svc/Update/' + info);
+}
+
+function loginClient(email, password) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(config.apiUrl + '/Client.svc/Authenticate/' + email + '/'+ password , requestOptions)
+            .then(handleResponse, handleError);
+            .then(user => {
+            // login successful if there's a jwt token in the response
+                if (user && user.token) {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('client', JSON.stringify(user));
+                }
+
+                return user;
+        });
+}
+
+function deleteClient(index) {
+    return postRequest('/Client.svc/Delete/' + index);
+}
+
+function logout(isClient) {
+    // remove user from local storage to log user out
+    var item = isClient ? 'client' : 'groomer';
+    localStorage.removeItem(item);
+}
+
+// 
+
+
+function postRequest (String info) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(config.apiUrl + info , requestOptions).then(handleResponse, handleError);
 }
 
 function logout() {
     // remove user from local storage to log user out
     localStorage.removeItem('user');
-}
-
-function getAll() {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
-
-    return fetch(config.apiUrl + '/users', requestOptions).then(handleResponse, handleError);
-}
-
-function getById(id) {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
-
-    return fetch(config.apiUrl + '/users/' + _id, requestOptions).then(handleResponse, handleError);
-}
-
-function register(user) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(user)
-    };
-
-    return fetch(config.apiUrl + '/users/register', requestOptions).then(handleResponse, handleError);
-}
-
-function update(user) {
-    const requestOptions = {
-        method: 'PUT',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify(user)
-    };
-
-    return fetch(config.apiUrl + '/users/' + user.id, requestOptions).then(handleResponse, handleError);
-}
-
-// prefixed function name with underscore because delete is a reserved word in javascript
-function _delete(id) {
-    const requestOptions = {
-        method: 'DELETE',
-        headers: authHeader()
-    };
-
-    return fetch(config.apiUrl + '/users/' + id, requestOptions).then(handleResponse, handleError);
 }
 
 function handleResponse(response) {
